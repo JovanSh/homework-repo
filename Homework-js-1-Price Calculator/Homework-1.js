6a9ec56c5f2633581eb9df8391a9dd4ba46a7a76
JovanSh <jovansapcevski0@gmail.com>
2023-12-06 04:57:21 +0100

    Finished Homework-1

---
+++ Homework-js-1-Price Calculator/Homework-1.js
@@ -0,0 +1,9 @@
+console.log("This is working");
+
+let initialPhonePrice = 119.95;
+
+let taxRate = 5;
+
+let phonePrice = (taxRate / 100) * initialPhonePrice * 30;
+
+console.log("The price of 30 phones is", phonePrice);
